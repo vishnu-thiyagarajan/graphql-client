b70304951faa25b1cc619859168cc6c8f02eefd2
Vishnu Thiyagarajan <vishnu.thg@gmail.com>
2021-12-06 01:33:35 +0530

    package version fix

--- src/util/DynamicRoute.js
+++ src/util/DynamicRoute.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 
-import { useAuthState } from '../context/auth'
+import { useAuthState } from "../context/auth";
 
 export default function DynamicRoute(props) {
-  const { user } = useAuthState()
+  const { user } = useAuthState();
 
   if (props.authenticated && !user) {
-    return <Redirect to="/login" />
+    return <Redirect to="/login" />;
   } else if (props.guest && user) {
-    return <Redirect to="/" />
+    return <Redirect to="/" />;
   } else {
-    return <Route component={props.component} {...props} />
+    return <Route component={props.component} {...props} />;
   }
 }
